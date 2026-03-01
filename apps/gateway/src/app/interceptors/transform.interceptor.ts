ba1a810e9a2377dcd9c3a231b72c7b0b7ceb1abe
Daniel Sukenik <sukenik9@gmail.com>
2026-03-01 19:31:38 +0200

    Added ValidationPipe and TransformInterceptor

---
+++ apps/gateway/src/app/interceptors/transform.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+export interface Response<T> {
+	data: T
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+	intercept(_: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+		return next.handle().pipe(
+			map((data) => ({
+				success: true,
+				data,
+				timestamp: new Date().toISOString()
+			}))
+		)
+	}
+}
