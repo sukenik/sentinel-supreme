887bcd4db5c9c905e2e1d2fba8b578ecdef6ad08
Daniel Sukenik <sukenik9@gmail.com>
2026-03-08 18:18:37 +0200

    Added registration

---
+++ apps/gateway/src/app/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common'
+import { RegisterDto } from '@sentinel-supreme/shared'
+import { UsersService } from './users.service'
+
+@Controller('users')
+export class UsersController {
+	constructor(private readonly usersService: UsersService) {}
+
+	@Post('register')
+	async register(@Body() body: RegisterDto) {
+		return this.usersService.create(body.email, body.password)
+	}
+}
---
+++ apps/gateway/src/app/users/users.service.ts
@@ -0,0 +1,35 @@
+import { ConflictException, Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import * as bcrypt from 'bcrypt'
+import { Repository } from 'typeorm'
+import { User } from './entities/user.entity'
+
+@Injectable()
+export class UsersService {
+	constructor(
+		@InjectRepository(User)
+		private readonly usersRepository: Repository<User>
+	) {}
+
+	async create(email: string, pass: string): Promise<User> {
+		const existingUser = await this.usersRepository.findOne({ where: { email } })
+
+		if (existingUser) {
+			throw new ConflictException('User already exists')
+		}
+
+		const salt = await bcrypt.genSalt()
+		const hashedPassword = await bcrypt.hash(pass, salt)
+
+		const newUser = this.usersRepository.create({
+			email,
+			password: hashedPassword
+		})
+
+		return this.usersRepository.save(newUser)
+	}
+
+	async getByEmail(email: string): Promise<User | null> {
+		return this.usersRepository.findOne({ where: { email } })
+	}
+}
