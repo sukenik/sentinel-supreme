ab7a1e208c9871bbc9c2d8f57d40f9f98244c84a
Daniel Sukenik <sukenik9@gmail.com>
2026-04-08 14:06:57 +0300

    Added rmq validation

--- apps/notification-service/src/app/app.module.ts
+++ apps/notification-service/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from 'joi'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationSchema: Joi.object({
+				RMQ_USER: Joi.string().required(),
+				RMQ_PASSWORD: Joi.string().required(),
+				RMQ_PORT: Joi.number().required(),
+				RMQ_VHOST: Joi.string().required(),
+				RMQ_HOST: Joi.string().required()
+			})
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
--- apps/notification-service/src/app/app.service.ts
+++ apps/notification-service/src/app/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { SharedRmqModule, validateRmqTopology } from '@sentinel-supreme/shared/server'
 
 @Injectable()
-export class AppService {
-	getData(): { message: string } {
-		return { message: 'Hello API' }
+export class AppService implements OnModuleInit {
+	private readonly logger = new Logger(AppService.name)
+
+	constructor(private readonly config: ConfigService) {}
+
+	async onModuleInit() {
+		await this.setupRabbitMQ()
+	}
+
+	private async setupRabbitMQ() {
+		try {
+			const rmqUrl = SharedRmqModule.getUrl(this.config)
+
+			await validateRmqTopology(rmqUrl)
+
+			this.logger.log('✅ RabbitMQ Topology (Exchanges/Queues) verified and created.')
+		} catch (error) {
+			this.logger.error('❌ Failed to setup RabbitMQ topology', error)
+		}
 	}
 }
