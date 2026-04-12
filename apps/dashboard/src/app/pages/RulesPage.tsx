9046a3f6023324f0ab635917774bfc1bef4716c6
Daniel Sukenik <sukenik9@gmail.com>
2026-04-12 21:19:05 +0300

    Added all reference

--- apps/dashboard/src/app/pages/RulesPage.tsx
+++ apps/dashboard/src/app/pages/RulesPage.tsx
@@ -24,8 +24,10 @@ const RulesPage: FC = () => {
 			[eSeverity.LOW]: 'bg-blue-500/10 text-blue-400 border border-blue-500/20',
 			[eSeverity.MEDIUM]: 'bg-yellow-500/10 text-yellow-400 border border-yellow-500/20',
 			[eSeverity.HIGH]: 'bg-orange-500/10 text-orange-400 border border-orange-500/20',
-			[eSeverity.CRITICAL]: 'bg-red-500/10 text-red-400 border border-red-500/20'
+			[eSeverity.CRITICAL]: 'bg-red-500/10 text-red-400 border border-red-500/20',
+			[eSeverity.ALL]: ''
 		}
+
 		return colors[sev]
 	}
 
