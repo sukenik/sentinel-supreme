3e7c6c40ed651ef4b92283c20e1b6ef4c54286e6
Daniel Sukenik <sukenik9@gmail.com>
2026-04-20 19:39:07 +0300

    Added guards

--- libs/shared/src/lib/ai-config/ai-config.controller.ts
+++ libs/shared/src/lib/ai-config/ai-config.controller.ts
@@ -1,9 +1,15 @@
-import { Body, Controller, Get, Param, Patch } from '@nestjs/common'
+import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common'
 import { GATEWAY_ROUTES } from '../consts'
+import { Roles } from '../decorators/roles.decorator'
+import { JwtAuthGuard } from '../guards/jwt-auth.guard'
+import { RolesGuard } from '../guards/roles.guard'
+import { eUserRole } from '../types'
 import { AiConfigService } from './ai-config.service'
 import { AiConfigEntity } from './entities/ai-config.entity'
 
 @Controller(GATEWAY_ROUTES.AI_CONFIG)
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(eUserRole.ADMIN)
 export class AiConfigController {
 	constructor(private readonly aiConfigService: AiConfigService) {}
 
